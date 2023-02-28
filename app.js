3b12a22879bf8aecd64ce648798f8b7765a59290
awolter27 <awolter@butler.edu>
2023-02-27 21:30:43 -0500

    Initial JS Setup

---
+++ app.js
@@ -0,0 +1,21 @@
+const suits = ['♣', '♦', '♥', '♠'];
+const values = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'];
+
+class Cards {
+    constructor() {
+        this.suit = suit,
+        this.value = value
+    }
+}
+
+class Player {
+    constructor() {
+
+    }
+} 
+
+class Computer {
+    constructor() {
+        
+    }
+}
