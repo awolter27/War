f701d128642aecdd8e60324e9d28cfd815215384
awolter27 <awolter@butler.edu>
2023-02-28 00:38:31 -0500

    HTML/CSS updates to design, JS updates for buttons

--- app.js
+++ app.js
@@ -1,3 +1,8 @@
+const playBtn = document.querySelector(.play);
+const restartBtn = document.querySelector(.restart);
+const drawBtn = document.querySelector(.draw);
+const warBtn = document.querySelector(.go-to-war);
+
 const suits = ['♣', '♦', '♥', '♠'];
 const values = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'];
 
