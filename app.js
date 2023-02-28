8fda5a096df2ecdf616f009c0c1f0ea1bd0ad108
awolter27 <awolter@butler.edu>
2023-02-28 03:42:47 -0500

    JS updates to variables and functions

--- app.js
+++ app.js
@@ -3,32 +3,35 @@ const restartBtn = document.querySelector('.restart');
 const drawBtn = document.querySelector('.draw');
 const warBtn = document.querySelector('.go-to-war');
 
-const suits = ['♣', '♦', '♥', '♠'];
 const values = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'];
-// let deck = [];
+const suits = ['♣', '♦', '♥', '♠'];
+const deck = [];
 
 let computerDeckDown = document.querySelector('.computer-deck-down');
 let playerDeckDown  = document.querySelector('.player-deck-down');
 let winner = document.querySelector('.winner');
 
-// function deckOfCards() {
-//     let card;
-//     for (let i = 0; i < suits.length; i++) {
-//         for(let j = 0; j < values.length; j++) {
-//             deck.push([i][j]);
-//         }
-//     }
-//     return deck;
-// }
-// console.log(deck);
+function deckOfCards() {
+    let cards = []
+    for (let i = 0; i < values.length; i++) {
+        for (let j = 0; j < suits.length; j++) {
+            cards = `${values[i]}${suits[j]}`
+            deck.push(cards);
+        }
+    }
+    return deck;
+}
 
 function play() {
+    // after the play button is clicked, it should randomly place half the deck into each players personal decks
+    computerDeckDown.innerHTML = 'Cards In Deck: 26';
+    playerDeckDown.innerHTML = 'Cards In Deck: 26';
     showDrawBtn();
 }
 
 function restart() {
-    computerDeckDown.innerHTML = 'Cards In Deck: 26';
-    playerDeckDown.innerHTML = 'Cards In Deck: 26';
+    computerDeckDown.innerHTML = 'Cards In Deck: 0';
+    playerDeckDown.innerHTML = 'Cards In Deck: 0';
     // reset computerDeckUp to nothing
     // reset playerDeckUp to nothing
     winner.innerHTML = 'Winner:';
